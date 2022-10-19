4c8b427eb6d2b37a3fd88b988586f091d7151584
Alixia <lizziejones1208@gmail.com>
2022-10-19 21:21:48 +0800

    Add link section for projects

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const header = document.querySelector('.header');
 const footer = document.querySelector('.footer');
 const main = document.querySelector('.main');
 const title = document.querySelector('h1');
+const links = document.querySelector('.linkcontainer');
 
 nightBtn.addEventListener('click', () => {
     header.classList.toggle('nightmode')
     footer.classList.toggle('nightmode')
     main.classList.toggle('mainNightmode')
+    links.classList.toggle('mainNightmode')
     nightBtn.classList.toggle('nightmodeBtnSwitch')
+    
 })
